0a940c379e7bb7f65ea4c51bbaa35da56708cec8
jsmnegrc <jsmne.dublado@gmail.com>
2023-09-06 08:52:37 +0800

    changes

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const menu = document.querySelector("#menu");
-const header = document.querySelector("header");
-
-menu.addEventListener("click", function () {
-  header.classList.toggle("open");
-});
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+};
+cancelBtn.onclick = () => {
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
